826099f515daa30968647514e5d7de7e4cc416d6
Michael Chan <michchandev@gmail.com>
2020-02-24 19:00:28 +0800

    Fix getPathname type

--- src/routing/getPathname.ts
+++ src/routing/getPathname.ts
@@ -2,7 +2,7 @@ import { LocationDescriptorObject, Location } from 'history';
 import isObj from '../object/isObj'
 
 
-const getPathname = (param: LocationDescriptorObject | Location): LocationDescriptorObject['pathname'] => (
+const getPathname = (param: LocationDescriptorObject | Location | string): LocationDescriptorObject['pathname'] => (
     isObj<LocationDescriptorObject>(param)
         ? param.pathname 
         : param
