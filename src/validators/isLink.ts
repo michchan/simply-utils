33b2af565b0bd7088fcf2de4261589e79c143cd8
Michael Chan <michchandev@gmail.com>
2020-03-18 16:12:50 +0800

    Update isLink to remove upper limit of url domain name

--- src/validators/isLink.ts
+++ src/validators/isLink.ts
@@ -1,6 +1,6 @@
 
 // Reference: https://www.regextester.com/93652
-const REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+const REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,}(:[0-9]{1,5})?(\/.*)?$/
 
 const isLink = (str: string): boolean => REGEX.test(str)
 
