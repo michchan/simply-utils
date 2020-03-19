a0995195d3924002251d34002029001fb7981863
Michael Chan <michchandev@gmail.com>
2020-03-19 22:29:17 +0800

    Add getSearchParam and getSearchParams

---
+++ src/routing/getSearchParam.ts
@@ -0,0 +1,41 @@
+import { Location, LocationDescriptor } from 'history';
+import isNum from '../number/isNum';
+import fromQueryString from '../network/fromQueryString';
+import getSearchParams from './getSearchParams';
+
+
+
+/**
+ * Get single query parameter from location
+ * 
+ * @param location 
+ * @param key 
+ * @param defaultValue 
+ * @param numberExpected 
+ * @param useWindowLocationAsFallback 
+ */
+function getSearchParam <T extends string | number = string> (
+    location: LocationDescriptor | Location = window.location, 
+    key: string | number,
+    defaultValue: T,
+    numberExpected?: boolean,
+    // Since argument 'location' can be string pathname, it might not necessarily contains search parameters.
+    // If this argument equals true, 
+    // that means window.location.search will be used as the last fallback of deriving search parameters.
+    useWindowLocationAsFallback: boolean = true,
+): T {
+    const search: null | string = getSearchParams(location, useWindowLocationAsFallback)
+    
+    if (search === null) return defaultValue
+        
+    const { [key]: value } = fromQueryString<{ [k in typeof key]?: string }>(search)
+
+    if (numberExpected) {
+        // To ensure return number string 
+        // if the value of searchObj[key] is number followed by any string
+        return (isNum(value, true) ? parseInt(value) : defaultValue) as T
+    }
+    return value as T
+}
+
+export default getSearchParam
---
+++ src/routing/getSearchParams.ts
@@ -0,0 +1,34 @@
+import isObj from '../object/isObj'
+import { Location, LocationDescriptor } from "history"
+
+
+/**
+ * Get query string from location
+ * 
+ * @param location 
+ * @param useWindowLocationAsFallback 
+ */
+const getSearchParams = (
+    location: Location | LocationDescriptor = window.location,
+    // Since argument 'location' can be string pathname, it might not necessarily contains search parameters.
+    // If this argument equals true, 
+    // that means window.location.search will be used as the last fallback of deriving search parameters.
+    useWindowLocationAsFallback: boolean = true,
+): null | string => {
+    // Return search from location object
+    if (isObj(location)) {
+        return location.search
+    }
+    // Return search from location string
+    const search = location.split('?')[1]
+    if (search) 
+        return search
+    // Return search from window.location object
+    if (useWindowLocationAsFallback) {
+        return window.location.search
+    }
+    // Return null
+    return null
+}
+
+export default getSearchParams
