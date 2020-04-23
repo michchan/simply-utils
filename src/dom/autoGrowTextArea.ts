e1cd945c4e9a80c544907c58311445a39577308e
Michael Chan <michchandev@gmail.com>
2020-04-23 16:13:14 +0800

    Remove requestAnimationFrame from autoGrowTextArea

--- src/dom/autoGrowTextArea.ts
+++ src/dom/autoGrowTextArea.ts
@@ -4,9 +4,9 @@
  * Auto-grow textarea according to content size
  * Reference: https://stackoverflow.com/questions/17772260/textarea-auto-height
  */
-const autoGrowTextArea = (el: HTMLTextAreaElement) => requestAnimationFrame(() => {
+const autoGrowTextArea = (el: HTMLTextAreaElement) => {
     el.style.height = `0px`;
     el.style.height = `${el.scrollHeight + 1}px`;
-})
+}
 
 export default autoGrowTextArea
