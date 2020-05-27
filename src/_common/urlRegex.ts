246c133f0fb5ed9a3e28d62ea70e3a6779eb7332
Michael Chan <michchandev@gmail.com>
2020-05-27 18:53:44 +0800

    Include "&" for urlRegex

--- src/_common/urlRegex.ts
+++ src/_common/urlRegex.ts
@@ -1,14 +1,14 @@
 // Reference: https://regex101.com/r/fics9a/3
-// Special character: [A-Za-z0-9_\.\/\-~\?=]
+// Special character: [A-Za-z0-9_\.\/\-~\?=&]
 
-const STRICT = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,})$/
+const STRICT = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|www\.[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=&]{2,})$/
 
-const STRICT_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,})/g
+const STRICT_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|www\.[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=&]{2,})/g
 
 // * Can be without protocol (http/https) or cname (www)
-const OPTIONAL_WWW = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})$/
+const OPTIONAL_WWW = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})$/
 
-const OPTIONAL_WWW_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})/g
+const OPTIONAL_WWW_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=&]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})/g
 
 
 const urlRegex = {
