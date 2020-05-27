8fcf00c6f8acd5164d94e6ef52fc5e611d899162
Michael Chan <michchandev@gmail.com>
2020-05-27 12:12:25 +0800

    Fix url regex for special charaters

--- src/_common/urlRegex.ts
+++ src/_common/urlRegex.ts
@@ -1,10 +1,12 @@
 // Reference: https://regex101.com/r/fics9a/3
-const STRICT = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/
-const STRICT_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g
+const STRICT = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,})$/
+
+const STRICT_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,})/g
 
 // * Can be without protocol (http/https) or cname (www)
-const OPTIONAL_WWW = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})$/
-const OPTIONAL_WWW_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})/g
+const OPTIONAL_WWW = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})$/
+
+const OPTIONAL_WWW_GLOBAL = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[A-Za-z0-9_\.\/\-~\?=]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[A-Za-z0-9_\.\/\-~\?=]{2,}|(www\.)[a-zA-Z0-9]+\.[^\s\W]{2,}|(?!www)([a-zA-Z0-9]+)\.[^\s\W]{2,})/g
 
 
 const urlRegex = {
