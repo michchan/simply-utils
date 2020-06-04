57b8541a587ee554cec85cd9afec4b491b8c9a70
Michael Chan <michchandev@gmail.com>
2020-06-04 20:50:01 +0800

    Update isURLSafeStr regex

--- src/validators/isURLSafeStr.ts
+++ src/validators/isURLSafeStr.ts
@@ -1,7 +1,12 @@
 
-export const URL_SAFE_REGEX_STR = '[a-zA-Z0-9_-]+'
 
-const REGEX = new RegExp(`^${URL_SAFE_REGEX_STR}$`, 'i')
+
+const CHAR_NUM_STR = `a-zA-Z0-9`
+export const URL_SAFE_CHAR_REGEX_STR = `[${CHAR_NUM_STR}_-]`
+export const URL_SAFE_REGEX_STR = `${URL_SAFE_CHAR_REGEX_STR}+`
+
+// The start and the end must be characters of alphabets or numbers
+const REGEX = new RegExp(`^[${CHAR_NUM_STR}]${URL_SAFE_REGEX_STR}[${CHAR_NUM_STR}]$`, 'i')
 
 /**
  * Whether it is an ID
