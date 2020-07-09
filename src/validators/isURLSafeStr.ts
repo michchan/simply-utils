b922e1665326fc8d739b39513b98c1cb04ac8a63
Michael Chan <michchandev@gmail.com>
2020-07-09 11:07:57 +0800

    Fix isURLSafeStr not handling length <=2 bug

--- src/validators/isURLSafeStr.ts
+++ src/validators/isURLSafeStr.ts
@@ -5,8 +5,13 @@ const CHAR_NUM_STR = `a-zA-Z0-9`
 export const URL_SAFE_CHAR_REGEX_STR = `[${CHAR_NUM_STR}_-]`
 export const URL_SAFE_REGEX_STR = `${URL_SAFE_CHAR_REGEX_STR}+`
 
+// Case 1. length >= 3
+const CASE_1 = `[${CHAR_NUM_STR}]${URL_SAFE_REGEX_STR}[${CHAR_NUM_STR}]`
+// Case 2. length <= 2
+const CASE_2 = `[${CHAR_NUM_STR}]{1,2}`
+
 // The start and the end must be characters of alphabets or numbers
-const REGEX = new RegExp(`^[${CHAR_NUM_STR}]${URL_SAFE_REGEX_STR}[${CHAR_NUM_STR}]$`, 'i')
+const REGEX = new RegExp(`^(${CASE_1}|${CASE_2})$`, 'i')
 
 /**
  * Whether it is an ID
