b60c8173810e3cd0912e834c32129bdbb168181d
Michael Chan <michchandev@gmail.com>
2020-08-05 14:29:57 +0800

    Add `parseCSSNumWithUnit`

---
+++ src/style/parseCSSNumWithUnit.ts
@@ -0,0 +1,20 @@
+import isNum from "../number/isNum"
+
+
+
+/**
+ * 
+ * @param value 
+ * @param unit Default to `px`
+ */
+function parseCSSNumWithUnit <
+    T extends number | string, 
+    R extends string
+> (
+    value: T,
+    unit: string = 'px'
+): R {
+    return (isNum(value, true) ? `${value}px` : value) as R
+}
+
+export default parseCSSNumWithUnit
