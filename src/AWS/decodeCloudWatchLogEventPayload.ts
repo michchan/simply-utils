29beba371b5fe171003ccfa424bb6d7b2592883e
Michael Chan <michchandev@gmail.com>
2020-10-13 22:45:17 +0800

    Add decodeCloudWatchLogEventPayload

---
+++ src/AWS/decodeCloudWatchLogEventPayload.ts
@@ -0,0 +1,11 @@
+import { CloudWatchLogsDecodedData, CloudWatchLogsEvent, CloudWatchLogsHandler } from "aws-lambda"
+import zlib from 'zlib'
+
+
+const decodeCloudWatchLogEventPayload = (event: CloudWatchLogsEvent): CloudWatchLogsDecodedData => {
+    const compressedPayload = Buffer.from(event.awslogs.data, 'base64')
+    const uncompressedPayload = zlib.unzipSync(compressedPayload).toString()
+    return JSON.parse(uncompressedPayload)
+}
+
+export default decodeCloudWatchLogEventPayload
