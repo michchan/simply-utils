a48cbc20eacf5bbe0ca885ed7c93389c8af9f754
Michael Chan <michchandev@gmail.com>
2020-10-23 16:25:29 +0800

    Upgrade dependencies

--- src/network/fromQueryString.ts
+++ src/network/fromQueryString.ts
@@ -1,4 +1,4 @@
-import qs from 'qs'
+import qs, { ParsedQs } from 'qs'
 
 
 /**
@@ -7,8 +7,13 @@ import qs from 'qs'
  * @param str Query string (?a=1&b=2)
  * @param fallback The fallback object
  */
-function fromQueryString <T extends object = object> (str: string, fallback: object = {}): T {
-  return qs.parse(/^\?/.test(str)? str.substr(1) : str) || fallback
+function fromQueryString <T extends ParsedQs = ParsedQs> (
+  str: string,
+  fallback: T = {} as T,
+): T {
+  return (qs.parse(
+    /^\?/.test(str) ? str.substr(1) : str
+  ) || fallback) as T
 }
 
 export default fromQueryString
