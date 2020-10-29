f83f37b401a4a3ffaf1b7a4cd6cc5af84bf32ade
Michael Chan <michchandev@gmail.com>
2020-10-29 12:46:01 +0800

    Add usage to pipeAsync

--- src/async/pipeAsync.ts
+++ src/async/pipeAsync.ts
@@ -1,3 +1,15 @@
+/**
+ * Usage:
+ * const asyncFunctions: (() => void)[] = Array(6)
+ *  .map((v, i) => (input: string) => new Promise(resovle => {
+ *    setTimeout(() => {
+ *      const nextInput = `${input}-${i}`
+ *      console.log('Resolve', i, input, nextInput)
+ *      resolve(nextInput)
+ *    }, 1000)
+ *  }))
+ * pipeAsync(...asyncFunctions)('Input to the first function')
+ */
 const pipeAsync = (...functions: any[]) => (input: unknown): any => functions
   .reduce((chain, func) => chain.then(func), Promise.resolve(input))
 
