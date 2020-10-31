95fa115e3350358b8cc447f1582264323e3c02f3
Michael Chan <michchandev@gmail.com>
2020-11-01 00:29:17 +0800

    Fix pipeAsync type errors

--- src/async/pipeAsync.ts
+++ src/async/pipeAsync.ts
@@ -3,7 +3,7 @@ type FuncType <O> = (...args: any[]) => Promise<O>
  * Usage:
  * ```
  * const asyncFunctions: (() => void)[] = Array(6)
- *  .map((v, i) => (input: string) => new Promise(resovle => {
+ *  .map((v, i) => (input: string) => new Promise(resolve => {
  *    setTimeout(() => {
  *      const nextInput = `${input}-${i}`
  *      console.log('Resolve', i, input, nextInput)
@@ -16,7 +16,7 @@ type FuncType <O> = (...args: any[]) => Promise<O>
 const pipeAsync = <
   O,
   T extends FuncType<O> = FuncType<O>
-> (...functions: T[]) => (input?: unknown): Promise<O[]> => functions
-  .reduce((chain, func) => chain.then(func), Promise.resolve(input)) as Promise<O[]>
+> (...functions: T[]) => (input?: O): Promise<O> => functions
+  .reduce((chain, func) => chain.then(func), Promise.resolve(input)) as Promise<O>
 
 export default pipeAsync
