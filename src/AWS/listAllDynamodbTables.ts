aa65d311b04a952527079f64e251d309a3c095e5
Michael Chan <michchandev@gmail.com>
2020-11-01 23:05:16 +0800

    Update options for listAllDynamodbTables

--- src/AWS/listAllDynamodbTables.ts
+++ src/AWS/listAllDynamodbTables.ts
@@ -5,51 +5,54 @@ export type ListAllTablesResult = DynamoDB.TableNameList
 
 export interface ListAllDynamodbTablesOptions {
   Limit?: DynamoDB.ListTablesInput['Limit'];
-  accTableNames?: DynamoDB.TableNameList;
   /** Delay between each query request. Default to 0 */
   delay?: number;
+  /** Other input to override */
+  input?: Partial<DynamoDB.ListTablesInput>;
+  /**
+   * Accumulated table names to be concentenated with the result.
+   * Default to an empty array.
+   */
+  accTableNames?: DynamoDB.TableNameList;
 }
-
 /**
  * List table recursively
  */
 const listAllDynamodbTables = (
   dynamodb: Pick<DynamoDB, 'listTables'>,
   ExclusiveStartTableName: string,
-  {
-    Limit,
-    accTableNames = [],
-    delay = 0,
-  }: ListAllDynamodbTablesOptions = {}
+  options: ListAllDynamodbTablesOptions = {}
 ): Promise<ListAllTablesResult> => new Promise((resolve, reject) => {
+  const {
+    Limit,
+    delay = 0,
+    input,
+    accTableNames = [],
+  } = options
+
   dynamodb.listTables({
     ExclusiveStartTableName,
     Limit,
+    ...input,
   }, async (err, data) => {
     if (err) {
       reject(new Error(`Unable to list tables. Error JSON: ${err}`))
     } else {
       const { TableNames = [], LastEvaluatedTableName } = data
-      const mergedTableNames = [...accTableNames, ...TableNames]
+      const concatedTableNames = [...accTableNames, ...TableNames]
 
       if (LastEvaluatedTableName) {
         if (delay > 0) await wait(delay)
         // Recur next
-        resolve(await listAllDynamodbTables(
-          dynamodb,
-          ExclusiveStartTableName,
-          {
-            Limit,
-            accTableNames: mergedTableNames,
-            delay,
-          }
-        ))
+        resolve(await listAllDynamodbTables(dynamodb, ExclusiveStartTableName, {
+          ...options,
+          accTableNames: concatedTableNames,
+        }))
       } else {
         // End recur
-        resolve(mergedTableNames)
+        resolve(concatedTableNames)
       }
     }
   })
 })
-
 export default listAllDynamodbTables
