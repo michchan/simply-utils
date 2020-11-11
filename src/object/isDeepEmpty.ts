4140ea1fd662f9fef23a54b3174093eff82e976f
Michael Chan <michchandev@gmail.com>
2020-11-11 12:51:48 +0800

    Create isDeepEmpty

---
+++ src/object/isDeepEmpty.ts
@@ -0,0 +1,16 @@
+import isEmpty from 'lodash/isEmpty'
+import { isArr } from '../array'
+import isObj from './isObj'
+
+/**
+ * Like lodash/isEmpty but also cater empty nested object and array cases.
+ * @param o
+ */
+const isDeepEmpty = (o: unknown): boolean => {
+  if (isObj(o))
+    return Object.keys(o).length === 0 || Object.values(o).every(val => isDeepEmpty(val))
+  if (isArr(o))
+    return o.length === 0 || o.every(item => isDeepEmpty(item))
+  return isEmpty(o)
+}
+export default isDeepEmpty
