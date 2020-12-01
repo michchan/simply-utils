1a506895a2e3632139274a2d474964958785b676
Michael Chan <michchandev@gmail.com>
2020-12-01 18:43:06 +0800

    Create composeEnv for dotEnv

---
+++ src/devOps/composeEnv.ts
@@ -0,0 +1,38 @@
+import pick from 'lodash/pick'
+
+// Config dot env
+require('dotenv').config()
+
+export type EnvValues <K extends string> = {
+  [key in K]: string;
+}
+
+export interface ComposeEnvReturnType <K extends string> {
+  keys: readonly K[];
+  values: EnvValues<K>;
+}
+
+/**
+ * Environment variables (dotenv) composer with empty check
+ */
+const composeEnv = <K extends string> (keys: readonly K[]): ComposeEnvReturnType<K> => {
+  const values = pick(process.env, keys) as unknown as EnvValues<K>
+  /**
+   * Throw an error if any of the environment variables is not defined
+   */
+  (() => {
+    const missingKeys: K[] = []
+    for (const key of keys) {
+      const value = values[key]
+      if (value === undefined) missingKeys.push(key)
+    }
+    if (missingKeys.length > 0)
+      throw new Error(`Value(s) undefined but required in environment: ${missingKeys.join(', ')}`)
+  })()
+
+  return {
+    keys,
+    values,
+  }
+}
+export default composeEnv
