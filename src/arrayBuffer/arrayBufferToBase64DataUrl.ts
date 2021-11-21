c13899543699dc077f90766943f81178b46dda4d
Michael Chan <michchandev@gmail.com>
2021-11-21 17:33:21 +0800

    feat: add arrayBufferToBase64DataUrl

---
+++ src/arrayBuffer/arrayBufferToBase64DataUrl.ts
@@ -0,0 +1,13 @@
+/**
+ * !! Caution: runtime will be blocked if using this for a large buffer (large file like 1GB)
+ *
+ * @param buf The array buffer data
+ * @param contentType The MIME type .e.g text/plain
+ */
+export function arrayBufferToBase64DataUrl (buf: ArrayBuffer, contentType: string): string {
+  const base64 = Buffer.from(buf).toString('base64')
+  const dataUrl = `data:${contentType};base64, ${encodeURIComponent(base64)}`
+  return dataUrl
+}
+
+export default arrayBufferToBase64DataUrl
--- src/arrayBuffer/index.ts
+++ src/arrayBuffer/index.ts
@@ -1,3 +1,4 @@
+export { default as arrayBufferToBase64DataUrl } from './arrayBufferToBase64DataUrl'
 export { default as arrayBufferToBlobUrl } from './arrayBufferToBlobUrl'
 export { default as arrayBufferToString } from './arrayBufferToString'
 export { default as stringToArrayBuffer } from './stringToArrayBuffer'
